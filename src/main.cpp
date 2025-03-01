30c4a5a96aa4502a0996d5d22dd0ca9ff1e00860
Xuming <voicevon@gmail.com>
2025-03-01 20:12:08 +0800

    第一次提交

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+#define  PIN_LED 19
+#define  PIN_LED_B 18
+#define PIN_MOTOR_A 13 
+#define PIN_MOTOR_B 14 
+#define PIN_MOTOR_C 33 
+#define PIN_MOTOR_F 32 
+
+  // 如果高电平，会关闭自己（ESP32掉电)。
+#define PIN_POWER 25  // 控制总供电。
+#define PIN_TOUCH 26  // 软件自定义功能
+
+  // 蜂鸣器，低电平 响。
+#define PIN_BUZZER 21
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
